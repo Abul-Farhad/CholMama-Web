1a2bd1612170bcab1df55712873ee5e61efea555
Abul-Farhad <abulfarhad.ibnalam@gmail.com>
2025-01-02 12:22:22 +0600

    landing page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,32 @@
 import { createBrowserRouter } from "react-router";
 import UserRegistrationContainer from "./modules/user-registration/container/UserRegistrationContainer";
 import UserLoginContainer from "./modules/user-login/container/UserLoginContainer";
+import DashBoard from "./modules/landing-page/container/DashBoard";
+import MainLayout from "./shared/components/layout/MainLayout";
 
 export const routes = createBrowserRouter([
     {
-        path: "/",
-        Component: () => <UserRegistrationContainer/>,
+        path: '/',
+        element: <MainLayout/>,
+        children:[
+            {
+                path: "/",
+                element: <UserRegistrationContainer/>,
+            },
+            {
+                path: "/login",
+                element: <UserLoginContainer/>
+            },
+            {
+                path: "/register",
+                element: <UserRegistrationContainer/>
+            },
+        ],
+        
     },
     {
-        path: "/login",
-        Component: () => <UserLoginContainer/>
+        path: "/dashboard",
+        element: <DashBoard/>
     }
     
 ])
